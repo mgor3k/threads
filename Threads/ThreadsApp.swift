c47c5fb5b7df626f0e5f78d045b967ab65f24095
Maciej Górecki <maciekgorecki@icloud.com>
2024-03-08 19:04:29 +0100

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Maciej Gorecki on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
