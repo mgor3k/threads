e09902e8567b95b7cc74fc9facc462c032f14b17
Maciej Górecki <maciekgorecki@icloud.com>
2024-03-11 17:46:54 +0100

    add navigation bar.

--- Threads/Features/New/NewThreadView.swift
+++ Threads/Features/New/NewThreadView.swift
@@ -3,8 +3,39 @@
 import SwiftUI
 
 struct NewThreadView: View {
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            content
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                        .tint(.white)
+                    }
+
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {}) {
+                            Image(systemName: "ellipsis.circle")
+                        }
+                        .tint(.white)
+                    }
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("New thread")
+        }
+    }
+
+    var content: some View {
+        VStack {
+            Divider()
+            
+            Text("Create post")
+
+            Spacer()
+        }
     }
 }
 
